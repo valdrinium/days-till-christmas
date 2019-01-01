3da2a6bb8f7f9189d6e538c15d54f11722f8f5b4
Valentin Nita <valdrinit@gmail.com>
2019-01-01 20:36:07 +0200

    Python Windows

--- src/repeat.py
+++ src/repeat.py
@@ -9,10 +9,10 @@ from utils.config import config
 def spam(arg):
     real_dir_path = os.path.realpath(os.path.dirname(__file__))
 
-    subprocess.run(['python', '-B', os.path.join(real_dir_path, 'images', 'download.py')])
-    subprocess.run(['python', '-B', os.path.join(real_dir_path, 'images', 'edit.py')])
+    subprocess.run(['pythonw', '-B' , os.path.join(real_dir_path, 'images', 'download.py')])
+    subprocess.run(['pythonw', '-B', os.path.join(real_dir_path, 'images', 'edit.py')])
     for victim in config('spam.victims'):
-        subprocess.run(['python', '-B', os.path.join(real_dir_path, 'spam.py'), victim])
+        subprocess.run(['pythonw', '-B' ,os.path.join(real_dir_path, 'spam.py'), victim])
 
 
 schedule.every().day.at(config('spam.spamAt')).do(spam, 'Let there be Christmas')
